f6e844acf94a7645a6aa3ae61944d6ab5b08b460
lester <lester@192.168.1.2>
2020-06-20 14:45:53 -0500

    Primer comit desde mi Sts hacia mi repo

---
+++ SpringDemo/src/test/java/pacifico/edu/pe/Demo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pacifico.edu.pe.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
